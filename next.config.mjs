25a472b4900824b94392928499452c588965b252
Karm Patel <karm.patel0315@gmail.com>
2025-05-24 00:30:06 -0700

    Changes to next.config.mjs to allow github pages to render it properly

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {unoptimized: true,}
+  output: "export",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/karmp.github.io" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/karmp.github.io/" : "",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
